78a483e755efce6bc617a311dcd64f31c0032bed
jpiccirillo <piccirilloj1@gmail.com>
2018-06-16 14:14:46 -0500

    adding in initial files including blank app.py flask application

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World! This is the updated version of the application.'
+
+if __name__ == "__main__":
+    app.run()
