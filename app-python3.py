98931bfb9e21665076a3ecae05cedf54346436cf
Jon <jonathan.koser@gmail.com>
2018-06-18 22:30:28 -0500

    Python3 version of the app
    Fixed some syntax and import errors and it works like a charm.

---
+++ app-python3.py
@@ -0,0 +1,87 @@
+from flask import Flask, session, render_template, request
+import os
+import urllib3
+import json
+import datetime
+import time
+import pytz
+import psycopg2
+import re
+import html.parser
+
+dbname = "testDB"
+user = "postgres"
+password = "postgres"
+conn_string = "dbname={} user={} password={}".format(dbname, user, password)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    session['logged_in'] = True
+    data = refreshData()
+    return render_template('home.html', entries=data)
+
+
+app.secret_key = os.urandom(12)
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return render_template("logout.html")
+
+
+@app.route('/api/insert', methods=['POST', 'GET'])
+def insertUser():
+    print("Logged In: ", session.get('logged_in'))
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
+    firstname = request.args.get("firstname")
+    lastname = request.args.get("lastname")
+
+    db = psycopg2.connect(conn_string)
+    cur = db.cursor()
+    sql = "INSERT INTO ListOfNames (first_name, last_name, age) VALUES (%s,%s,%s)"
+    insert_data = (firstname, lastname, 5)
+    try:
+        cur.execute(sql, insert_data)
+        db.commit()
+
+    except Exception as e:
+        print(insert_data)
+        print(e)
+
+    return "success"
+
+
+# @app.route('api/refresh'):
+def refreshData():
+    db = None
+    data = []
+    try:
+        db = psycopg2.connect(conn_string)
+        cur = db.cursor()
+        sql = "SELECT * FROM ListOfNames ORDER BY id DESC"
+        cur.execute(sql)
+
+        rows = cur.fetchall()
+        print("The number of rows: ", cur.rowcount)
+        for row in rows:
+            data.append(row)
+            print(row)
+        print(data)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if db is not None:
+            db.close()
+        return data
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
