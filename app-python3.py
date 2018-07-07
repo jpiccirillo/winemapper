8f4834fae80e27d90ae7c1085ec37d0fa0d99bd7
jpiccirillo <piccirilloj1@gmail.com>
2018-07-07 14:51:19 -0500

    developing the front end w login/logout pages, navbar

--- app-python3.py
+++ app-python3.py
@@ -18,18 +18,73 @@ conn_string = "host={} port={} dbname={} user={} password={}".format(host, port,
 
 app = Flask(__name__)
 
+# @app.route('/', methods=['POST', 'GET'])
+# def home():
+#     if not session.get('logged_in'):
+#         return render_template('login.html')
+#     if session.get('is_power'):
+#         return render_template('powerHome.html')
+#     else:
+#         return render_template('home.html')
+#        #if session.get('is_regular'):
+#         #   return render_template('home.html')
+#        #if session.get('is_power'):
+#         #   return render_template('powerHome.html')
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
+    # if not session.get('logged_in'):
+    #     return render_template('login.html')
+    #
+    # else:
     session['logged_in'] = True
     data = refreshData()
     return render_template('browse.html', entries=json.dumps(data))
 
 app.secret_key = os.urandom(12)
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template("login.html")
+#    global UID
+#    username = request.form['username']
+#    print(username)
+#    password = request.form['password']
+#    print(password)
+#    con = sql.connect(db)
+#    con.row_factory = sql.Row
+#    cur = con.cursor()
+#
+#    query = "SELECT * FROM User WHERE Username=\'" + username + "\'"
+#    cur.execute(query)
+#    rows = cur.fetchall()
+#
+#    if len(rows) == 0:
+#       return home()
+#
+#    elif helper_function.checkPassword(password.encode(), rows[0]['Password'].encode()):
+#        if rows[0]['UserLevel'] == 'power':
+#           session['is_power'] = True
+#        elif rows[0]['UserLevel'] == '':
+#           session['is_regular'] = True
+#        session['logged_in'] = True
+#        session['username'] = username
+#        UID = rows[0]['UID']
+#       #print("UID: ", UID)
+#
+#        return home()
+#
+#    else:
+#       return home()
+
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
     return render_template("logout.html")
+
+@app.route('/climate')
+def showPlot():
+    return render_template("climate.html")
 
 @app.route('/api/insert', methods=['POST', 'GET'])
 def insertUser():
@@ -69,7 +124,7 @@ def refreshData():
         for row in rows:
             data.append(row)
             print(row)
-        print(data)
+        # print(data)
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
